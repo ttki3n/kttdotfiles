d73351d7d8c3aaac2884caa22ad5e24c355b95c0
Tran Trung Kien2 <kien.trantrung2@gameloft.com>
2026-07-14 16:03:54 +0700

    tweak a bit

--- dotfiles/create_symlink.bat
+++ dotfiles/create_symlink.bat
@@ -11,6 +11,6 @@ mklink /d "%USERPROFILE%\.config\starship" "%~dp0starship"
 mklink /d "%USERPROFILE%\.config\wezterm" "%~dp0wezterm"
 
 mklink /d "%APPDATA%\yazi\config" "%~dp0yazi"
-
+setx YAZI_FILE_ONE "C:\Git\usr\bin\file.exe"
 
 pause
