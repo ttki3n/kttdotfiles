496e118ab83d4501c09a4c7892d2612e709f295b
KienTT <ttki3n@gmail>
2026-08-15 23:58:45 +0700

    change yazi save projects method.

--- dotfiles/yazi/init.lua
+++ dotfiles/yazi/init.lua
@@ -22,9 +22,9 @@ require("projects"):setup({
 		},
 	},
 	save = {
-		method = "yazi", -- yazi | lua
-		yazi_load_event = "@projects-load", -- event name when loading projects in `yazi` method
-		lua_save_path = "", -- path of saved file in `lua` method, comment out or assign explicitly
+		method = "lua", -- yazi | lua
+		-- yazi_load_event = "@projects-load", -- event name when loading projects in `yazi` method
+		-- lua_save_path = "", -- path of saved file in `lua` method, comment out or assign explicitly
 		-- default value:
 		-- windows: "%APPDATA%/yazi/state/projects.json"
 		-- unix: "~/.local/state/yazi/projects.json"
